9e02bb57fc2e47279125935e4d04d6f41d9c7de4
spencexyz <mccullough.spencer@gmail.com>
2016-07-11 17:45:56 +0200

    new landing page

---
+++ scripts.js
@@ -0,0 +1,24 @@
+setTimeout(function(){
+  document.getElementById("bgvid").play();
+}, 5000);
+
+//controls the fade in
+var el = document.querySelector('#bgvid');
+setTimeout(function(){
+  if (el.classList.contains('is-paused')){
+    el.classList.remove('is-paused');
+  }
+}, 5000);
+
+var countDiv = document.getElementById('countdown');
+var count = 4;
+
+var interval = setInterval(function(){ 
+  if(count > 0) {
+    countDiv.innerHTML = count;
+    count--;
+  } else {
+    countDiv.innerHTML = "Get It!";
+    clearInterval(interval);
+  }
+}, 1000);
